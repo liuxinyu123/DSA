26a936117def2181c512427ec885c46373347c1a
liuyakun <happy_lucky52@163.com>
2016-11-01 22:50:23 +0800

    伸展树的实现

--- splaytree/splaytree.cpp
+++ splaytree/splaytree.cpp
@@ -49,7 +49,12 @@ Position FindMax(SplayTree t)
 
 Position Find(ElemType value,SplayTree t)
 {
-
+	if(!t || t -> element == value)
+		return t;
+	else if(value < t -> element)
+		return Find(value,t -> left);
+	else
+		return Find(value,t -> right);
 }
 
 void InOrder(SplayTree t)
@@ -68,4 +73,85 @@ ElemType Retrieve(Position p)
 		return p -> element;
 	else
 		return (ElemType)0;
+}
+
+SplayTree Insert(ElemType value,SplayTree t)
+{
+	t = InsertValue(value,t);
+	
+}
+
+SplayTree InsertValue(ElemType value,SplayTree t)
+{
+	Position p = (Position)malloc(sizeof(struct SplayTreeNode));
+
+	if(!p)
+	{
+		std::cout << "Memory for alloc for SplayTreeNode is failed!" << std::endl;
+		return nullptr;
+	}
+	p -> element = value;
+	p -> left = p -> right = nullptr;
+
+	if(!t)
+		return p;
+	else if(value < t -> element)
+		t -> left = InsertValue(value,t -> left);
+	else if(t -> element < value)
+		t -> right = InsertValue(value,t -> right);
+	
+	return t;//相等不做处理
+}
+
+Position Splay(ElemType value,SplayTree t)
+{
+	if(!t || t -> element == value)//空树或者等于根的情况
+		return t;
+
+	Position p = Find(value,t);//查找位置
+	
+	if(!p)
+		return t;
+
+	while()
+}
+
+Position ZigZigLeft(Position t,Position p,Position g)
+{
+	g -> left = p -> right;
+	p -> left = t -> right;
+	t -> right = p;
+	p -> right = g;
+
+	return t;
+}
+
+Position ZigZigRight(Position t,Position p,Position g)
+{
+	g -> right = p -> left;
+	p -> right = t -> left;
+	t -> left = p;
+	p -> left = g;
+
+	return t;
+}
+
+Position ZigZagLeft(Position t,Position p,Position g)
+{
+	g -> left = t -> right;
+	p ->right = t -> left;
+	t -> left = p;
+	t -> right = g;
+
+	return t;
+}
+
+Position ZigZagRight(Position t,Position p,Position g)
+{
+	g -> right = t -> left;
+	p -> left = t -> right;
+	t -> left = g;
+	t -> right = p;
+
+	return t;
 }
--- splaytree/splaytree.h
+++ splaytree/splaytree.h
@@ -12,8 +12,13 @@ Position FindMin(SplayTree t);
 Position FindMax(SplayTree t);
 Position Find(ElemType value,SplayTree t);
 SplayTree Insert(ElemType value,SplayTree t);
+Position InsertValue(ElemType value,SplayTree t);
 SplayTree Delete(ElemType value,SplayTree t);
 void InOrder(SplayTree t);
 ElemType Retrieve(Position p);
-
+Position Splay(ElemType value,SplayTree t);
+Position ZigZigLeft(Position t,Position p,Position g);
+Position ZigZigRight(Position t,Position p,Position g);
+Position ZigZagLeft(Position t,Position p,Position g);
+Position ZigZagRight(Position t,Position p,Position g);
 #endif
